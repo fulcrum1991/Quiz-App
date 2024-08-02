3d3ca5f563925439b44b06c853725bd10f1fe197
MarvinSchilling <marvin.schilling@iu-study.org>
2024-08-02 14:54:16 +0200

    Views erweitert und UI verbessert

--- Quiz-App/Library/forms.py
+++ Quiz-App/Library/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 from .models import QuizTask
 
 
@@ -6,4 +9,10 @@ class QuizTaskForm(forms.ModelForm):
     class Meta:
         model = QuizTask
         fields = ['question']
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    role = forms.ChoiceField(choices=[('student', 'Student'), ('dozent', 'Dozent')], widget=forms.RadioSelect)
 
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'role', 'password1', 'password2')
