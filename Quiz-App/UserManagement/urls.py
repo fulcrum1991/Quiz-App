3387f16d1cc26ed3ac18fdff0c043557d773edac
fulcrum1991 <kaiser.mario91@gmail.com>
2024-08-03 03:44:24 -0700

    Trennung von Library und UserManagement. Jetzt jeweils in eigener App.

--- Quiz-App/UserManagement/urls.py
+++ Quiz-App/UserManagement/urls.py
@@ -4,4 +4,6 @@ from UserManagement import views
 
 urlpatterns = [
     path('sign-up', views.sign_up, name='sign_up'),
+    path('profile/', views.profile, name='profile'),
+    path('delete_profile/', views.delete_profile, name='delete_profile'),
 ]
