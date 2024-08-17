e2079fc4aa1f41ef06f81f4dbe7b117458c3a105
fulcrum1991 <kaiser.mario91@gmail.com>
2024-08-17 08:06:55 -0700

    Testing

--- Quiz-App/Quiz-App/settings.py
+++ Quiz-App/Quiz-App/settings.py
@@ -31,10 +31,13 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
+DEBUG = True
 
 
 ALLOWED_HOSTS = ["*"]
+
+if 'CODESPACE_NAME' in os.environ:
+    CSRF_TRUSTED_ORIGINS = [f'https://{os.getenv("CODESPACE_NAME")}-8000.{os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")}']
 
 # Application definition
 
