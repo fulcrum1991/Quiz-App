76b8a2da94f9c5ca4adf7ce16f5dd88e2998ab8b
fulcrum1991 <kaiser.mario91@gmail.com>
2024-08-17 09:30:40 -0700

    Testing

--- Quiz-App/Quiz-App/wsgi.py
+++ Quiz-App/Quiz-App/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Quiz-App.settings')
+settings_module = 'Quiz-App.production' if 'WEBSITE_HOSTNAME' in os.environ else 'Quiz-App.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
