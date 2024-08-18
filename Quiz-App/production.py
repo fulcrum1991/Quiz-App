2537866b5be752aab93f5670de0b12f4824bea43
fulcrum1991 <kaiser.mario91@gmail.com>
2024-08-18 02:43:41 -0700

    Testing

--- Quiz-App/production.py
+++ Quiz-App/production.py
@@ -1,7 +1,7 @@
 import os
 
-from .settings import *  # noqa
-from .settings import BASE_DIR
+from settings import *  # noqa
+from settings import BASE_DIR
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
