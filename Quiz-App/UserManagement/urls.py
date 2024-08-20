654d90ec5dff8e129131d41cfbbdde7876e04a3a
MarvinSchilling <marvin.schilling@iu-study.org>
2024-08-20 10:03:59 +0200

    URLS korrigiert

--- Quiz-App/UserManagement/urls.py
+++ Quiz-App/UserManagement/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('sign-up', views.sign_up, name='sign_up'),
     path('accounts/login/', views.login_view, name='login'),
     path('accounts/logout/', views.logout_view, name='logout'),
-    path('profile/', views.profile_view, name='profile'),
-    path('edit-profile/', views.edit_profile, name='edit-profile'),
-    path('delete-profile/', views.delete_profile, name='delete-profile'),
+    path('accounts/profile/', views.profile_view, name='profile'),
+    path('accounts/profile_edit_form/', views.edit_profile, name='edit-profile'),
+    path('accounts/delete-profile/', views.delete_profile, name='delete-profile'),
 ]
