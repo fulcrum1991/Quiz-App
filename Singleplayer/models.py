06792db22d668df53c198dcf40c76e5af889a775
MarioK <96475736+fulcrum1991@users.noreply.github.com>
2024-08-21 23:30:05 -0700

    Singleplay\models implementiert. Gemäß ERM.

--- Singleplayer/models.py
+++ Singleplayer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from Library.models import QuizTask
 
 # Create your models here.
+class Quiz(models.Model):
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    completed_at = models.DateTimeField()
+
+class Quiz_contains_Quiztask(models.Model):
+    quiz_id = models.ForeignKey(Quiz, on_delete=models.CASCADE)          # check
+    task_id = models.ForeignKey(QuizTask, on_delete=models.CASCADE)      # check
+    correct_answered = models.BooleanField()
+    completed = models.BooleanField()
